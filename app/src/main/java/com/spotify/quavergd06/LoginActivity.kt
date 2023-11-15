6dcc290ef59d31c2f71f01453218fdc83ff974d8
Juan <juanboticario@gmail.com>
2023-11-15 18:23:13 +0100

    GD06-31 HU01.3: Acceso al Token de inicio de sesión

--- app/src/main/java/com/spotify/quavergd06/LoginActivity.kt
+++ app/src/main/java/com/spotify/quavergd06/LoginActivity.kt
@@ -1,11 +1,63 @@
 package com.spotify.quavergd06
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.View
+
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+
+import com.spotify.quavergd06.databinding.ActivityLoginBinding
+
 class LoginActivity : AppCompatActivity() {
+
+    private val redirectUri: Uri
+        get() = Uri.Builder()
+            .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
+            .authority(getString(R.string.com_spotify_sdk_redirect_host))
+            .build()
+
+    companion object {
+        const val CLIENT_ID = "e14071702cea4db5bf9255924ec9e882"
+        const val AUTH_TOKEN_REQUEST_CODE = 0x10
+    }
+
+    private var mAccessToken: String? = null
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onRequestTokenClicked(view: View?) {
+        val request = getAuthenticationRequestToken()
+        AuthorizationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request)
+    }
+
+    private fun getAuthenticationRequestToken(): AuthorizationRequest {
+        return AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, redirectUri.toString())
+            .setShowDialog(false)
+            .setScopes(arrayOf("user-read-email"))
+            .setCampaign("your-campaign-token")
+            .build()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response = AuthorizationClient.getResponse(resultCode, data)
+
+        if (requestCode == AUTH_TOKEN_REQUEST_CODE) {
+            mAccessToken = response.accessToken
+        }
     }
 }
