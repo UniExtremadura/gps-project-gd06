49b9d3df1707937df6a5e2c87e2af8af1004fb67
jorgegg02 <jorgegg02.jgg@gmail.com>
2023-11-17 18:01:40 +0100

    GD06-55 HU03.2 Implementar la captura de imagen desde cámara o galería

--- app/src/main/java/com/spotify/quavergd06/view/home/MomentEditFragment.kt
+++ app/src/main/java/com/spotify/quavergd06/view/home/MomentEditFragment.kt
@@ -1,60 +1,116 @@
 package com.spotify.quavergd06.view.home
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.spotify.quavergd06.model.Moment
+import java.text.SimpleDateFormat
+import java.util.Locale
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.Manifest
+import android.content.pm.PackageManager
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 import com.spotify.quavergd06.R
+import com.spotify.quavergd06.databinding.FragmentMomentEditBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MomentEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MomentEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentMomentEditBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_moment_edit, container, false)
+    ): View {
+        _binding = FragmentMomentEditBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MomentEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MomentEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val moment = arguments?.getSerializable("moment") as? Moment
+        //TODO: Adecuar la carga según el origen de los datos
+        if (moment != null) {
+            moment?.let {
+                // Configurar la vista con los detalles del Momento
+                binding.detailImage.setImageResource(it.image)
+                binding.detailSongTitle.setText(it.songTitle)
+                binding.detailLocation.text = it.location
+
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val formattedDate = dateFormat.format(it.date)
+                binding.detailDate.text = formattedDate// ...
+
+                binding.detailTitle.setText(it.title)
+                // Configurar otros elementos según sea necesario
             }
+        } else {
+            openCamera()
+        }
+
+
+    }
+    override fun onResume() {
+        super.onResume()
+
+        // Ocultar BottomNavigationView
+        val navBar: BottomNavigationView? = activity?.findViewById(R.id.bottom_navigation)
+        navBar?.visibility = View.GONE
+    }
+
+    private fun openCamera() {
+        // Verificar si se tienen los permisos necesarios
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Solicitar permisos si no están otorgados
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            // Abrir la cámara
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as? Bitmap
+            binding.detailImage.setImageBitmap(imageBitmap)
+        }
+    }
+
+    // ...
+
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 123
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 456
+    }
+    override fun onPause() {
+        super.onPause()
+
+        // Mostrar BottomNavigationView
+        val navBar: BottomNavigationView? = activity?.findViewById(R.id.bottom_navigation)
+        navBar?.visibility = View.VISIBLE
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
