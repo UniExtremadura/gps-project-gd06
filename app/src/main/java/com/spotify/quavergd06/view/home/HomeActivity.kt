424887084397a07674a16520f50a0478b0f86ab5
FelixExDe <felixexpositoduran@gmailcom>
2023-11-20 21:30:10 +0100

    GD06-121 HU09.2 Diseño de la interfaz para notificaciones

--- app/src/main/java/com/spotify/quavergd06/view/home/HomeActivity.kt
+++ app/src/main/java/com/spotify/quavergd06/view/home/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.spotify.quavergd06.view.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.spotify.quavergd06.R
 import com.spotify.quavergd06.databinding.ActivityHomeBinding
+import com.spotify.quavergd06.notifications.NotificationScheduler
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -20,7 +26,32 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setUpUI()
+        createChannel()
+
+
     }
+
+    private fun createChannel() {
+        Log.d("NotificationScheduler", "Setting reminder")
+
+        NotificationScheduler.setReminder(this, NotificationScheduler::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                MY_CHANNEL_ID,
+                "MySuperChannel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "SUSCRIBETE"
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
     private fun setUpUI() {
         val navHostFragment =
