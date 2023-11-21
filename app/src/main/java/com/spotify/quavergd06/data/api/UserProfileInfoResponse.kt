0e7c889b2830461f6789228fb00c6aad7f75ea89
FelixExDe <felixexpositoduran@gmailcom>
2023-11-21 19:50:07 +0100

    GD06-126 HU02.2 Diseño y colocación de la opción de cerrar sesión

---
+++ app/src/main/java/com/spotify/quavergd06/data/api/UserProfileInfoResponse.kt
@@ -0,0 +1,7 @@
+package com.spotify.quavergd06.data.api
+import com.google.gson.annotations.SerializedName
+
+data class UserProfileInfoResponse (
+    @SerializedName("display_name"     ) var displayName     : String?           = null,
+    @SerializedName("images"           ) var images          : ArrayList<ApiImage> = arrayListOf(),
+)
