584de7585117990445457217b18a6dfd209837eb
JuanBoticario <juanboticario@gmail.com>
2023-11-21 19:31:09 +0100

    GD06-125 HU02.1 Implementar la funcionalidad de cerrar sesión

--- app/src/main/java/com/spotify/quavergd06/view/home/ProfileFragment.kt
+++ app/src/main/java/com/spotify/quavergd06/view/home/ProfileFragment.kt
@@ -1,22 +1,24 @@
 package com.spotify.quavergd06.view.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.spotify.quavergd06.R
+import com.spotify.quavergd06.databinding.FragmentProfileBinding
+import com.spotify.quavergd06.view.LoginActivity
+import com.squareup.picasso.Picasso
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
 
-
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -24,7 +26,42 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        val logoutButton = binding.unlogButton
+        logoutButton.setOnClickListener {
+            // Borrar el token almacenado
+            clearUserToken()
+        }
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Obtén la ImageView
+        val pictureImageView = binding.AvatarImageView
+
+        // con picaso muestra la imagen de perfil del usuario que se el drawable spotify_logo.png
+        Picasso.get().load(R.drawable.spotify_logo).into(pictureImageView)
+
+        // Obtén el TextView
+        val usernameTextView = binding.usernameTextView
+        // Obtén el nombre de usuario utilizando la función getUsername()
+        val username = getUserName()
+        // Establece el nombre de usuario en el TextView
+        usernameTextView.text = username
+    }
+
+
+    private fun clearUserToken() {
+        val sharedPreferences =
+            requireContext().getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit().clear().apply()
+    }
+
+    private fun getUserName():String {
+        return "User"
+    }
 }
