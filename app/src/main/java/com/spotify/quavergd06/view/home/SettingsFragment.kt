ef444e3302b9c2847a387f1ec80345c0b9a96b8a
FelixExDe <felixexpositoduran@gmailcom>
2023-11-24 16:54:29 +0100

    GD06-156 HU14.2 Implementar lógica para cambiar idioma

--- app/src/main/java/com/spotify/quavergd06/view/home/SettingsFragment.kt
+++ app/src/main/java/com/spotify/quavergd06/view/home/SettingsFragment.kt
@@ -5,6 +5,9 @@ import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.spotify.quavergd06.R
 import com.spotify.quavergd06.model.ThemeManager
+import java.util.Locale
+import android.content.res.Configuration
+import androidx.core.content.ContentProviderCompat.requireContext
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -23,6 +26,30 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val themePreference = findPreference<ListPreference>("theme_preference")
         val themeEntries = resources.getStringArray(R.array.theme_entries)
         themePreference?.entries = themeEntries
+
+        findPreference<ListPreference>("language_preference")?.setOnPreferenceChangeListener { _, newValue ->
+            // Handle language change
+            updateLocale(newValue as String)
+            true
+        }
+
+        // Update language entries with localized strings
+        val languagePreference = findPreference<ListPreference>("language_preference")
+        val languageEntries = resources.getStringArray(R.array.language_entries)
+        languagePreference?.entries = languageEntries
+
+    }
+
+    private fun updateLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val config = Configuration(resources.configuration)
+        config.setLocale(locale)
+
+        requireContext().resources.updateConfiguration(config, requireContext().resources.displayMetrics)
+
+        activity?.recreate()
     }
 
 }
