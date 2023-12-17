3762589a208fb7831d033d7d12c2393e22663999
Mateos <xavimateosm@gmail.com>
2023-12-17 19:58:17 +0100

    Top genres view model

--- app/src/main/java/com/spotify/quavergd06/view/home/stats/topGenres/TopGenresFragment.kt
+++ app/src/main/java/com/spotify/quavergd06/view/home/stats/topGenres/TopGenresFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.spotify.quavergd06.api.getNetworkService
@@ -14,29 +16,29 @@ import com.spotify.quavergd06.api.setKey
 import com.spotify.quavergd06.data.api.ArtistItem
 import com.spotify.quavergd06.data.model.Artist
 import com.spotify.quavergd06.data.toArtist
+import com.spotify.quavergd06.data.toStatsItem
 import com.spotify.quavergd06.databinding.FragmentTopGenresBinding
+import com.spotify.quavergd06.view.home.HomeViewModel
+import com.spotify.quavergd06.view.home.stats.StatsItemAdapter
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 
 class TopGenresFragment : Fragment() {
+    private val TAG = "TopGenresFragment"
+
     private var _binding: FragmentTopGenresBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var adapter: GenresAdapter
-    private var artists = emptyList<Artist>()
-    private var genres = emptyList<String>()
-    private var topGenres = emptyList<String>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val viewModel: TopGenresViewModel by viewModels { TopGenresViewModel.Factory }
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentTopGenresBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -45,60 +47,31 @@ class TopGenresFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyclerView()
 
-        // Realizar una búsqueda de artistas en Spotify
-        lifecycleScope.launch {
-            try {
-                setKey(obtenerSpotifyApiKey(requireContext())!!)
-                artists = fetch("long_term")
-                genres = getGenresFromArtistList(artists)
-
-                android.util.Log.d("TopGenresFragment", "Genres: $genres")
-
-                val topGenres = getTopGenres(genres)
-
-                android.util.Log.d("TopGenresFragment", "Top Genres: $topGenres")
-
-                adapter.updateData(topGenres)
-            } catch (e: Exception) {
-                Log.d("TopGenresFragment", "Error: ${e.message}")
-            }
+        homeViewModel.user.observe(viewLifecycleOwner) { user ->
+            viewModel.user = user
         }
+
+        subscribeUI(adapter)
     }
 
-    suspend fun fetch(timePeriod: String): List<Artist> {
-        val apiArtists = getNetworkService().loadTopArtists(timePeriod).body()?.artistItems ?: emptyList()
-        return apiArtists.map(ArtistItem::toArtist)
+    private fun subscribeUI(adapter: GenresAdapter) {
+        viewModel.genres?.observe(viewLifecycleOwner) { genres ->
+            adapter.updateData(genres)
+        }
     }
 
     private fun setUpRecyclerView() {
         adapter = GenresAdapter(
-            genres = genres,
+            genres = emptyList(),
             context = this.context
         )
         with(binding) {
-            topGenresRecyclerView.layoutManager = LinearLayoutManager(context, androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL,  false)
+            topGenresRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,  false)
             topGenresRecyclerView.adapter = adapter
         }
-        Log.d("TopGenresFragment", "setUpRecyclerView")
+        Log.d(TAG, "RecyclerView")
     }
 
-//    private fun getGenresFromArtistList(artists: List<Artist>): List<String> {
-//        return artists.flatMap { it.genres }
-//    }
-//
-    //Refactoriza getGenresFromArtistList para que se adecúe al cambio hecho con el StringListWrapper
-    private fun getGenresFromArtistList(artists: List<Artist>): List<String> {
-        return artists.flatMap { it.genres.list }
-    }
-
-    private fun getTopGenres(genres: List<String>): List<String> {
-        return genres.groupingBy { it }.eachCount().toList().sortedByDescending { (_, value) -> value }.map { (key, _) -> key }
-    }
-
-    private fun obtenerSpotifyApiKey(context: Context): String? {
-        val sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        return sharedPreferences.getString("access_token", null)
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
